2deb3f91c6ad93b52e26970870e303396cac549f
athul23p <athul238p@gmail.com>
2022-04-18 14:10:23 +0530

    initial commit

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,103 @@
+export const AddStudent = () => {
+  return (
+    <form className="addstudent">
+      <div>
+        Firstname:{" "}
+        <input
+          type="text"
+          name="first_name"
+          className="first_name"
+          placeholder="enter first name"
+        />
+      </div>
+      <div>
+        {" "}
+        Last Name:
+        <input
+          type="text"
+          name="last_name"
+          className="last_name"
+          placeholder="enter last name"
+        />
+      </div>
+      <div>
+        {" "}
+        Email:
+        <input
+          type="email"
+          name="email"
+          className="email"
+          placeholder="enter email"
+        />
+      </div>
+
+      <div>
+        Gender: {/* NOTE: radio boxes only work when they have same `name`. */}
+        <div>
+          Male
+          <input
+            name="gender"
+            className="male"
+            type="radio"
+            value={"male"}
+          />{" "}
+          Female{" "}
+          <input
+            name="gender"
+            className="female"
+            type="radio"
+            value={"female"}
+          />
+        </div>
+      </div>
+      <div>
+        Age{" "}
+        <input
+          type="number"
+          name="age"
+          className="age"
+          placeholder="enter age"
+        />
+      </div>
+      <div>
+        Tenth Score:{" "}
+        <input
+          type="number"
+          name="tenth_score"
+          className="tenth_score"
+          placeholder="enter 10th score"
+        />{" "}
+      </div>
+      <div>
+        Twelth Score:{" "}
+        <input
+          type="number"
+          name="twelth_score"
+          className="twelth_score"
+          placeholder="enter 12th score"
+        />{" "}
+      </div>
+      <div>
+        <select
+          value={""} // select dropdown needs both value and onChange attributes
+          name="preferred_branch"
+          className="preferred_branch"
+        >
+          <option value="law">law</option>
+          <option value="commerce">commerce</option>
+          <option value="science">science</option>
+          <option value="sports">sports</option>
+          <option value="arts">arts</option>
+          <option value="acting">acting</option>
+        </select>
+      </div>
+
+      <input className="submit" type="submit" value="Submit" />
+      {
+        // <div className="error"></div>
+        // show this div with proper error before submitting form, if there's anything not provided
+        // eg: first name missing, age cannot be greater than 100 etc
+      }
+    </form>
+  );
+};
