da96ab34324772ab8a180f31b9f35959230cb798
athul23p <athul238p@gmail.com>
2022-04-18 15:11:41 +0530

    Show student : - displays student data - sorts by age, & scores

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { AddStudent } from "./components/AddStudent";
 import { ShowStudents } from "./components/ShowStudents";
+import {useState} from 'react';
 
 function App() {
+  const [showStudents,setShowStudents] = useState(true);
+
   return (
     <div className="App">
-      <button className="togglebtn"></button>
+      <button className="togglebtn" onClick={() => setShowStudents(!showStudents)}>
+        {showStudents?"Go to form":"show students"}
+      </button>
+      {showStudents? <ShowStudents/> : <AddStudent/>}
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {/* make sure the table is shown initially, do not show form initially */}
       {/* make sure to show either of them do not both together */}
--- src/components/ShowStudents.jsx
+++ src/components/ShowStudents.jsx
@@ -1,4 +1,43 @@
+import './showstudents.css';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 export const ShowStudents = () => {
+  const [students,setStudents] = useState([]);
+  const [sortControls,setSortControls] = useState({
+    sortby:"first_name",
+    sortorder:"asc"
+  });
+  useEffect(() => {
+    getStudentData();
+  },[]);
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setSortControls({...sortControls,[name]:value})
+  }
+
+  const getStudentData = () =>{
+    axios.get("http://localhost:8080/students")
+    .then(response => {
+      console.log(response);
+      setStudents(response.data);
+    })
+    .catch(error => console.log(error));
+  }
+
+  const sortData = () => {
+    console.log("sort", sortControls.sortby);
+    const tempData = [...students];
+    tempData.sort((a,b) => {
+      if(sortControls.sortorder === 'asc'){
+        return a[sortControls.sortby] - b[sortControls.sortby];
+      }
+      else{
+        return b[sortControls.sortby] - a[sortControls.sortby];
+      }
+    })
+    setStudents(tempData);
+  }
   return (
     <div>
       <div className="controls">
@@ -7,8 +46,10 @@ export const ShowStudents = () => {
           <select
             // select dropdown needs both value and onChange
             className="sortby"
+            onChange={handleChange}
+            name="sortby"
           >
-            <option value="first_name">First Name</option>
+            <option value="first_name" selected>First Name</option>
             <option value="gender">Gender</option>
             <option value="age">Age</option>
             <option value="tenth_score">10th Score</option>
@@ -20,12 +61,14 @@ export const ShowStudents = () => {
           <select
             // select dropdown needs both value and onChange
             className="sortorder"
+            onChange = {handleChange}
+            name="sortorder"
           >
-            <option value="asc">Ascending</option>
+            <option value="asc" selected>Ascending</option>
             <option value="desc">Descending</option>
           </select>
         </div>
-        <button className="sort">sort</button>
+        <button className="sort" onClick={sortData}>sort</button>
       </div>
       <table className="table">
         <thead>
@@ -42,7 +85,8 @@ export const ShowStudents = () => {
         </thead>
         <tbody className="tbody">
           {/* populate all rows like below: */}
-          <tr className="row">
+
+          {/* <tr className="row">
             <td className="first_name"></td>
             <td className="last_name"></td>
             <td className="email"></td>
@@ -51,7 +95,21 @@ export const ShowStudents = () => {
             <td className="tenth_score"></td>
             <td className="twelth_score"></td>
             <td className="preferred_branch"></td>
-          </tr>
+          </tr> */}
+          { students.map(s => {
+            return (
+              <tr className="row">
+                <td className="first_name">{s.first_name}</td>
+                <td className="last_name">{s.last_name}</td>
+                <td className="email">{s.email}</td>
+                <td className="gender">{s.gender}</td>
+                <td className="age">{s.age}</td>
+                <td className="tenth_score">{s.tenth_score}</td>
+                <td className="twelth_score">{s.twelth_score}</td>
+                <td className="preferred_branch">{s.preferred_branch}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
